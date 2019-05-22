95474a2a3a2a2b803371be7a9c3a368084c333de
Joel Gonzalez <joel.gonzalez@ipointsystems.com>
2019-05-22 13:04:48 -0500

    fixed errors

--- server.js
+++ server.js
@@ -122,5 +122,5 @@ io.on('connection', (socket) => {
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function(){
-    console.log( "Listening on " + port );
+    console.log( "Listening");
 })
